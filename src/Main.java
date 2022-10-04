fd0651491187e2681589092b14c1f240ef19b8c1
Andrei <andreigalitianu@gmail.com>
2022-10-04 11:00:35 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ public class Main {
     {
         int n = args.length;
         float s=0;
-        int[] argList = new int[n];
         for(int i=0; i<n; i++)
-        {
-            argList[i] = Integer.parseInt(args[i]);
-            s+=argList[i];
-        }
+            s+= Integer.parseInt(args[i]);
+
         s=s/n;
         System.out.println(s);
     }
